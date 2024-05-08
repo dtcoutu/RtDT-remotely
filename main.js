d0b52f8ea5960c2428857bcc34129f36d1623010
David Coutu <david.coutu@gmail.com>
2024-05-08 08:26:04 -0400

    Add details for potions

--- main.js
+++ main.js
@@ -476,6 +476,39 @@ const GEAR = [
     },
 ]
 
+const POTIONS = [
+    {
+        id: 'fortunes-favor',
+        name: 'Potion of Fortune\'s Favor',
+        description: 'Spend to gain +1 Wild Advantage'
+    },
+    {
+        id: 'dragons-teeth',
+        name: 'Potion of Dragon\'s Teeth',
+        description: 'Spend to gain 6 <img src="icons/warrior.png" title="warrior" alt="warrior" width="18" height="18" />.'
+    },
+    {
+        id: 'golden-sun',
+        name: 'Potion of the Golden Sun',
+        description: 'Spend to gain 1 <img src="icons/spirit.png" title="spirit" alt="spirit" width="18" height="18" />.'
+    },
+    {
+        id: 'one-thousand-strides',
+        name: 'Potion of One Thousand Strides',
+        description: 'Spend to move any hero up to 3 spaces.'
+    },
+    {
+        id: 'sirens-song',
+        name: 'Potion of the Siren\'s Song',
+        description: 'Spend to move any for up to 2 spaces.'
+    },
+    {
+        id: 'purifying-breath',
+        name: 'Potion of Purifying Breath',
+        description: 'Spend to remove up to 2 skulls from any building.'
+    },
+]
+
 function resetGame() {
     localStorage.setItem(CHARACTER_STORAGE, null);
     localStorage.setItem(COMPANIONS_STORAGE, JSON.stringify([]));
