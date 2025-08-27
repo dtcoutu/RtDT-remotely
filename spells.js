f1b172b20e01d2422570167eb0d7f2028b2993be
David Coutu <david.coutu@gmail.com>
2025-08-26 20:34:51 -0400

    Add details for spells for Reverent Astromancer

---
+++ spells.js
@@ -0,0 +1,62 @@
+export const SPELLS = [
+  {
+    id: 'aura-of-friendship',
+    name: 'Aura of Friendship',
+    type: 'spell',
+    description: 'When you Reinforce at a building this turn you can treat it as any building.'
+  },
+  {
+    id: 'bestow-blessing',
+    name: 'Bestow Blessing',
+    type: 'spell',
+    description: 'One hero gain a blessing.'
+  },
+  {
+    id: 'bounty-of-the-gods',
+    name: 'Bounty of the Gods',
+    type: 'spell',
+    description: 'Look at 3 random quest items. Gain 1. At the end of your turn, return it to the quest item deck.'
+  },
+  {
+    id: 'ritual-of-warding',
+    name: 'Ritual of Warding',
+    type: 'spell',
+    description: 'Place a protection token on your space.'
+  },
+  {
+    id: 'soothing-word',
+    name: 'Soothing Word',
+    type: 'spell',
+    description: 'One hero on your space replaces a corruption with a new one.'
+  },
+  {
+    id: 'winds-of-change',
+    name: 'Winds of Change',
+    type: 'spell',
+    description: 'Put any number of treasures from the market on the bottom of the treasure deck. Then, refill the market.'
+  },
+  {
+    id: 'abate-the-darkness',
+    name: 'Abate the Darkness',
+    type: 'invocation',
+    description: 'Remove a seal from teh Tower. Return any skulls that emerge to the supply. Replace the seal.'
+  },
+  {
+    id: 'celestial-jaunt',
+    name: 'Celestial Jaunt',
+    type: 'invocation',
+    description: 'Place your hero on any space with a quest marker.'
+  },
+  {
+    id: 'commanding-rebuke',
+    name: 'Commanding Rebuke',
+    type: 'invocation',
+    description: 'Place a foe that is on your space on any other space.'
+  },
+  {
+    id: 'smite-the-wicked',
+    name: 'Smite the Wicked',
+    type: 'invocation',
+    description: 'Remove a savage or lethal foe.'
+  },
+]
