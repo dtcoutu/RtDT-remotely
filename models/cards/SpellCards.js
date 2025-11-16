5581950ab30229cc40381f86c1a3bfd06664c8ef
David Coutu <david.coutu@gmail.com>
2025-11-16 15:26:02 -0500

    Update SpellCards.js

--- models/cards/SpellCards.js
+++ models/cards/SpellCards.js
@@ -8,7 +8,7 @@ export class SpellCards extends CardArray {
   }
 
   get cards() {
-    const allowedSpells = new Metadata().allowed_spells;
+    const allowedSpells = new Metadata().allowedSpells;
 
     return SPELLS.filter(spell => allowedSpells.includes(spell.type));
   }
